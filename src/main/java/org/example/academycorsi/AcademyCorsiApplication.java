92fb6efd0082417e094809e6c80ec92612a5ac37
Milo <m.zaffiri@elitesoftwarehouse.com>
2025-05-28 11:38:01 +0200

    inizio progetto

---
+++ src/main/java/org/example/academycorsi/AcademyCorsiApplication.java
@@ -0,0 +1,13 @@
+package org.example.academycorsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyCorsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyCorsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/academycorsi/AcademyCorsiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.academycorsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyCorsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
